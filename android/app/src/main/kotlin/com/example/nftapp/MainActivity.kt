d731f0d74ea541a4121bbbbe5942ad49e52a70d5
r31 <padariyavrund725@gmail.com>
2024-05-21 11:51:19 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/nftapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.nftapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:nftapp/app/views/splash_screen/Splash_screen.dart';
+
+import 'app/views/intro_screen/views/intro.dart';
+
+void main() {
+  runApp(
+    GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      getPages: [
+        GetPage(
+          name: "/",
+          page: () => const SplashScreen(),
+        ),
+        GetPage(
+          name: "/intr",
+          page: () => Intro(),
+        ),
+      ],
+    ),
+  );
+}
