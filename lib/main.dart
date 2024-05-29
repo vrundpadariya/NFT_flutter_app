05d221779d11d3c7caa76fba0c9fc40b03ee0fdf
vrundpadariya <padariyavrund725@gmail.com>
2024-05-29 08:59:42 -0400

    s

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,23 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:nftapp/app/views/splash_screen/Splash_screen.dart';
+import 'package:nftapp/login.dart';
 
 import 'app/views/intro_screen/views/intro.dart';
 
 void main() {
-  runApp(
-    GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      getPages: [
-        GetPage(
-          name: "/",
-          page: () => const SplashScreen(),
-        ),
-        GetPage(
-          name: "/intro",
-          page: () => Intro(),
-        ),
-      ],
-    ),
-  );
+  runApp(GetMaterialApp(
+    debugShowCheckedModeBanner: false,
+    getPages: [
+      GetPage(
+        name: "/",
+        page: () => const SplashScreen(),
+      ),
+      GetPage(
+        name: "/intro",
+        page: () => Intro(),
+      ),
+      GetPage(name: "/login", page: ()=>login_page(),)
+    ],
+  ),);
 }
